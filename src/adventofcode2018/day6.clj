a3cd58068fc5258f9d66ebb5fb54e2162dd36e22
Weston Neal <weston@edatadivers.com>
2018-12-07 14:40:24 -0700

    Continued work on day6.

--- src/adventofcode2018/day6.clj
+++ src/adventofcode2018/day6.clj
@@ -25,7 +25,31 @@
           [[500 500] [0 0]] ; defaults that will be replaced
           coords))
 
+(defn manhattan-distance
+  [m p]
+  (let [[mx my] m
+        [px py] p]
+    (+ (. Math abs (- mx px))
+       (. Math abs (- my py)))))
+
+(defn closest-points
+  [w h points]
+  (let [indexed-points (map-indexed vector points)]
+    (for [x (range w)
+          y (range h)]
+      (apply min-key #(manhattan-distance [x y] (second %))
+             indexed-points))))
+
 (defn challenge-a
-  [coords])
+  [coords]
+  (let [result (closest-points 400 400 coords)
+        [[x-min y-min] [x-max y-max]] (find-min-max coords)]
+        
+    (->> (frequencies result)
+         (filter (fn [[p _]]
+                   (let [[_ [p-x p-y]] p]
+                     (and (> x-max p-x x-min)
+                          (> y-max p-y y-min)))))
+         (apply max-key second))))
 
 (defn challenge-b [])
